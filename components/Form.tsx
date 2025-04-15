f9d67136ed49603fab831f5594fd2a51ac9d89b2
Fred <huertaserick@yahoo.ie>
2025-04-15 20:16:48 +0100

    TypeScript practice day 1

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,33 +4,73 @@ import { useState } from "react";
 const Form = () => {
   const [submitQuestion, setSubmitQuestion] = useState<{
     name: string;
-    student: boolean;
+    student: boolean | undefined;
   }>({
     name: "",
-    student: null,
+    student: undefined,
   });
 
-  const handleSubmit = () => {
-    setSubmitQuestion();
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    const { name, value } = e.target;
+
+    setSubmitQuestion((prev) => ({
+      ...prev,
+      [name]: name === "student" ? value === "true" : value,
+    }));
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form
-        onSubmit={handleSubmit}
-        className="container border p-3 bg-neutral-700 m-auto max-w-md flex flex-col justify-center items-center"
-      >
+      <form className="container border p-3 bg-neutral-700 m-auto max-w-md flex flex-col justify-center items-center">
         <fieldset className="border w-full p-3 rounded">
           <legend className="p-1 text-2xl">Tell us about yourself?</legend>
 
           <div className="flex flex-col justify-start max-w-sm space-y-1">
-            <label htmlFor="">What's your name?</label>
-            <input type="text" className="border p-1 bg-white" />
-            <label htmlFor="">Are you a student?</label>
-            <select name="" id="">
-              <option value="true">Yes</option>
-              <option value="false">False</option>
-            </select>
+            {/* name */}
+            <div className="flex flex-col">
+              <label htmlFor="name">What's your name?</label>
+              <input
+                id="name"
+                type="text"
+                value={submitQuestion.name}
+                onChange={handleChange}
+                className="border p-2 bg-white rounded text-black"
+              />
+            </div>
+
+            {/* student */}
+            <div>
+              <span className="mb-3">Are you a student?</span>
+              <div className="flex flex-col">
+                <label htmlFor="student-yes" className="">
+                  <input
+                    type="radio"
+                    value="true"
+                    onChange={handleChange}
+                    placeholder="Yes"
+                    className="mr-2"
+                    name="student"
+                    id="student"
+                  />
+                  Yes
+                </label>
+
+                <label htmlFor="student-no">
+                  <input
+                    value="false"
+                    type="radio"
+                    className="mr-2"
+                    name="student"
+                    id="student"
+                  />
+                  No
+                </label>
+              </div>
+            </div>
           </div>
         </fieldset>
 
