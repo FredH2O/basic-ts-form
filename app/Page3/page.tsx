1422c1a9f37ff4bf2a760c74c47de758e7967be5
Fred <huertaserick@yahoo.ie>
2025-05-06 18:22:44 +0100

    feat: add ApproachQuestion to Page3 folder and connected onChange + value in it

--- app/Page3/page.tsx
+++ app/Page3/page.tsx
@@ -3,6 +3,7 @@ import FormSection from "../../components/FormSection";
 import { useFormContext } from "../context/FormContext";
 import FrustrationQuestion from "../../components/Questions_3/FrustrationQuestion";
 import ChallangesQuestion from "../../components/Questions_3/ChallengesQuestion";
+import ApproachQuestion from "../../components/Questions_3/ApproachQuestion";
 const Page3 = () => {
   const { handleChange, handleSubmit, submitQuestion, otherAnswer } =
     useFormContext();
@@ -16,6 +17,10 @@ const Page3 = () => {
         onChange={handleChange}
         value={submitQuestion.challenges}
         otherAnswer={otherAnswer}
+      />
+      <ApproachQuestion
+        onChange={handleChange}
+        value={submitQuestion.approach}
       />
     </FormSection>
   );
